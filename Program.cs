f7d1b19ce7b3f94317a5dea3df8523d755d0d204
DontsovD <triggonayt@yandex.ru>
2022-09-17 15:08:18 +0300

    Task 4

--- Program.cs
+++ Program.cs
@@ -5,22 +5,52 @@
 // a = -9 b = -3 -> max = -3
 
 
+// Console.Clear();
+// Console.Write("Enter number1: ");
+
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Enter number2: ");
+
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+// {
+//     Console.WriteLine(num1 + " A larger number");
+//     Console.WriteLine(num2 + " Smaller number");
+// }
+// else
+// {
+//     Console.WriteLine(num2 + " A larger number");
+//     Console.WriteLine(num1 + " Smaller number");
+// }
+
+
+
+
+
+//  Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
 Console.Clear();
-Console.Write("Enter number1: ");
+Console.WriteLine("Enter number1: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number2: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number3: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
 
-int num1 = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Enter number2: ");
-
-int num2 = Convert.ToInt32(Console.ReadLine());
-
-if (num1 > num2)
+if (n1 >= n2)
 {
-    Console.WriteLine(num1 + " A larger number");
-    Console.WriteLine(num2 + " Smaller number");
+    if (n1 >= n3)
+        Console.WriteLine(n1 + "BIG");
 }
-else
+if (n2 >= n1)
 {
-    Console.WriteLine(num2 + " A larger number");
-    Console.WriteLine(num1 + " Smaller number");
+    if (n2 >= n3)
+        Console.WriteLine(n2 + "BIG");
+}
+if (n3 >= n2)
+{
+    if (n3 >= n1)
+        Console.WriteLine(n3 + "BIG");
 }
