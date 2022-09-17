fc7ea3c7b64334a1ff063138e132810542ada976
DontsovD <triggonayt@yandex.ru>
2022-09-17 15:55:16 +0300

    Task 6

--- Program.cs
+++ Program.cs
@@ -31,26 +31,46 @@
 
 //  Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.Clear();
-Console.WriteLine("Enter number1: ");
-int n1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter number2: ");
-int n2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter number3: ");
-int n3 = Convert.ToInt32(Console.ReadLine());
+// Console.Clear();
+// Console.WriteLine("Enter number1: ");
+// int n1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter number2: ");
+// int n2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter number3: ");
+// int n3 = Convert.ToInt32(Console.ReadLine());
 
-if (n1 >= n2)
+// if (n1 >= n2)
+// {
+//     if (n1 >= n3)
+//         Console.WriteLine(n1 + "BIG");
+// }
+// if (n2 >= n1)
+// {
+//     if (n2 >= n3)
+//         Console.WriteLine(n2 + "BIG");
+// }
+// if (n3 >= n2)
+// {
+//     if (n3 >= n1)
+//         Console.WriteLine(n3 + "BIG");
+// }
+
+
+
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+
+Console.Clear();
+Console.WriteLine("Enter number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+if ( n1 % 2 == 0)
 {
-    if (n1 >= n3)
-        Console.WriteLine(n1 + "BIG");
+    Console.WriteLine("Even");
 }
-if (n2 >= n1)
+else
 {
-    if (n2 >= n3)
-        Console.WriteLine(n2 + "BIG");
-}
-if (n3 >= n2)
-{
-    if (n3 >= n1)
-        Console.WriteLine(n3 + "BIG");
+    Console.WriteLine("Odd");
 }
